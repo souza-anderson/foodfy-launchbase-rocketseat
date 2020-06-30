09f07350b9d65f0aae9884842780fe400bfd23d6
andersonfebs <andersonfebs@gmail.com>
2020-06-30 06:09:58 -0400

    redirect unique page recipe

--- public/scripts.js
+++ public/scripts.js
@@ -3,22 +3,14 @@ const cards = document.querySelectorAll('.card');
 
 for (let card of cards) {    
     card.addEventListener('click', function() {
-        const modal = document.querySelector('.modal');
-        const image = card.querySelector('img').getAttribute('src');
-        const titleRecipe = card.querySelector('h2').innerHTML;
-        const authorRecipe = card.querySelector('p').innerHTML;
-
-        document.querySelector('.modal img').setAttribute('src', image);
-        document.querySelector('.modal h2').innerHTML = titleRecipe;
-        document.querySelector('.modal p').innerHTML = authorRecipe;
-
-        modalOverlay.classList.add('active');
+        const index = card.getAttribute("id");
+        window.location.href = `/recipes/${index}`;
     });
 }
 
-document.querySelector('.close-modal').addEventListener('click', function() {
-    modalOverlay.classList.remove('active');
-});
+// document.querySelector('.close-modal').addEventListener('click', function() {
+//     modalOverlay.classList.remove('active');
+// });
 
 
 
